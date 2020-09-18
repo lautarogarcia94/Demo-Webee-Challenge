17ea68ea8069227812a51d33476bb4b16a661887
Lautaro Garcia <lautygcr@gmail.com>
2020-09-18 01:34:19 -0300

    Add calls to Controller class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,16 @@
+import controller.Controller;
+
 import java.time.LocalDate;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-
-        Scanner myObj = new Scanner(System.in);
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        Controller controller = new Controller();
         Boolean repetir = true;
-        String macAddress, choice, id, date;
-
+        String macAddress;
+        String choice;
+        String id;
 
         do {
             System.out.println("What do you want to do? (enter a letter A, B ... E)");
@@ -21,29 +24,30 @@ public class App {
 
             switch (choice.toUpperCase()) {
                 case "A":
-                    System.out.println("\n"+"For register a new device entity, please enter the device date: (Date Format: DDMMYYYY)a");
-                    date = myObj.nextLine().toUpperCase();
+                    System.out.println("\n" + "For register a new device entity, please enter the device date: (Date Format: DDMMYYYY)a");
+                    String date = myObj.nextLine().toUpperCase();
                     System.out.println("Enter a MAC address: (MAC address Format: FF:FF:FF:FF:FF:FF)");
                     macAddress = myObj.nextLine().toUpperCase();
-                    System.out.println("Registering a device with date: "+date+" and MAC address: "+macAddress);
+                    System.out.println(controller.insertDevice(date, macAddress) + "\n");
                     break;
                 case "B":
-                    System.out.println("\n"+"The list of saved devices is:");
+                    System.out.println("\n" + "The list of saved devices is:");
+                    System.out.println(controller.getListDevice() + "\n");
                     break;
                 case "C":
-                    System.out.println("\n"+"Please enter a MAC address to look for a device:");
+                    System.out.println("\n" + "Please enter a MAC address to look for a device:");
                     macAddress = myObj.nextLine().toUpperCase();
-                    System.out.println("Looking for a device with MAC address: "+ macAddress);
+                    System.out.println(controller.getDeviceByMac(macAddress) + "\n");
                     break;
                 case "D":
-                    System.out.println("\n"+"Please enter an ID to look for a device:");
+                    System.out.println("\n" + "Please enter an ID to look for a device:");
                     id = myObj.nextLine().toUpperCase();
-                    System.out.println("Looking for a device with ID: "+ id);
+                    System.out.println(controller.getDeviceById(id) + "\n");
                     break;
                 case "E":
-                    System.out.println("\n"+"Please enter an ID to delete a device:");
+                    System.out.println("\n" + "Please enter an ID to delete a device:");
                     id = myObj.nextLine().toUpperCase();
-                    System.out.println("Deleting a device with ID: "+ id);
+                    System.out.println(controller.deleteDeviceById(id) + "\n");
                     break;
                 case "F":
                     System.out.println("Shooting down program...");
@@ -58,3 +62,4 @@ public class App {
 
     }
 }
+
